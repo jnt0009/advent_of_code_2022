4f8d6aa86a40d8b1bd4b7c76a85d9a0f72c5470c
jnt0009 <31008913+jnt0009@users.noreply.github.com>
2022-12-05 22:53:42 -0500

    ADD day 3. This got ugly

---
+++ day_3/solution.R
@@ -0,0 +1,109 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  "readr",
+  "dplyr"
+)
+
+library(readr)
+
+df <- read_csv("C:/Users/JamariusTaylor/Desktop/projects/advent_of_code/day_3/input.txt", 
+               col_names = FALSE)
+
+colnames(df) <- "sack"
+
+dict <- rbind(
+  data.frame(
+    key = letters,
+    value = 1:26
+  ),
+  data.frame(
+    key = LETTERS,
+    value = 27:52
+  )
+)
+
+i = df$sack[8]
+
+dupe_list <- c()
+final_count <- c()
+for (i in df$sack) {
+  
+  
+  chars <-  nchar(i) |> print()
+  
+  comp1 <- substr(i, 1, (chars/2)) |> print()
+  comp2 <- substr(i, (chars/2) + 1, chars) |> print()
+  
+  comp1 <- comp1 |> strsplit("") 
+  comp2 <- comp2 |> strsplit("") 
+  
+  comp1 <- comp1[[1]] |> unique() |> print()
+  comp2 <- comp2[[1]] |> unique() |> print()
+  
+  comp1 <- as.list(comp1)
+  comp2 <- as.list(comp2)
+  
+  dupes <- comp1[comp1 %in% comp2] |> unlist() |> print()
+  
+  val <- plyr::mapvalues(dupes,
+                  from = c(dict$key),
+                  to = c(dict$value)) |>
+    as.numeric() |> 
+    print()
+  # 
+  final_count <- append(final_count, val)
+  # 
+
+}
+
+
+#### Answer 1 
+final_count |> sum()
+
+y <- 1
+z <- ""
+tmp <- c()
+final <- c()
+for (i in df$sack) {
+  # print(i)
+  z = i |> strsplit("") |> unique() 
+  z = z[[1]] |> unique()
+  tmp[[y]] <- z  
+  
+  
+  
+  if(y == 3) {
+    y <- 1
+    step1 <- tmp[[1]][tmp[[1]] %in% tmp[[2]]]
+    step2 <- step1[step1 %in% tmp[[3]]]
+    final <- append(final, step2)
+
+    print(tmp)
+    # tmp <- c()
+    # print("over")
+  } else {
+    y <- y + 1
+    # z <- i |> strsplit("")
+    # z <- z[[1]] |> unique()  
+
+    # print("next")
+  }
+}
+ 
+
+#### Second Answer #### 
+plyr::mapvalues(final,
+                from = c(dict$key),
+                to = c(dict$value)) |>
+  as.numeric() |> 
+  sum()
+  print()
+ 
+
+
+
+
+
+
+
+ 
